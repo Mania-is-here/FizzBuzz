a783db1a095181fb4b6172f9ee7aa56abe5cf301
Detering <detering.mania@training.lug-ag.de>
2024-11-27 20:42:08 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int e = 1; e <= 100; e++)
+            {
+                if (e % 3 == 0 && e % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (e % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (e % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
