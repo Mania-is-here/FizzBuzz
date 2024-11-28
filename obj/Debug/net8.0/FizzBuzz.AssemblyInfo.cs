8c343fbbfe96fac0767717b2c5331bdfb0792274
Detering <detering.mania@training.lug-ag.de>
2024-11-28 10:18:54 +0100

    commiting

--- obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619f831196f5a3c11b1d82457a31a18f36b360c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f417295df71d4cb3f82221212e46e17405d1eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
